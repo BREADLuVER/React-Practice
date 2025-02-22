a4d1e55786cc66f5f0fbebb6f1ac1d239ed1149b
BREADLuVER <ry2050@nyu.edu>
2025-02-22 11:47:48 -0500

    ClickContext

---
+++ adClickTracker/ad-click-tracker/context/ClickContext.js
@@ -0,0 +1,23 @@
+import {createContext, useContext, useState} from 'react';
+
+const ClickContext = createContext(undefined);
+
+export const ClickProvider = ({children}) => {
+    const [clicks, setClicks] = useState(0);
+
+    const addClick = () => setClicks((prev) => prev+1);
+
+    return (
+        <ClickContext.ClickProvider values={{clicks, addClick}}>
+            {children}
+        </ClickContext.ClickProvider>
+    );
+};
+
+export const useClick = () => {
+    const context = useContext(ClickContext);
+    if (!context) {
+        throw new Error('No context')
+    }
+    return context
+}
